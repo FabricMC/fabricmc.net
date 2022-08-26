e913475ee73d7530ff273a8a5d9cb3fca5ab4af4
modmuss50 <modmuss50@gmail.com>
2022-08-26 18:01:10 +0000

    Deploying to static from  @ a8b1354567691f29bddc510ff278bdadb84d36e2 🚀

--- scripts/Api.js
+++ scripts/Api.js
@@ -1,50 +1,58 @@
-async function o() {
-  return n("https://meta.fabricmc.net/v2/versions/installer");
-}
-async function c() {
-  return n("https://meta.fabricmc.net/v2/versions/game");
-}
-async function f() {
-  return n("https://meta.fabricmc.net/v2/versions/loader");
-}
-async function m() {
-  return n("https://meta.fabricmc.net/v2/versions/yarn");
-}
-async function l(t, e) {
-  return n(`https://meta.fabricmc.net/v2/versions/loader/${t}/${e}/profile/json`);
+const r = ["https://meta.fabricmc.net", "https://meta2.fabricmc.net"], o = ["https://maven.fabricmc.net", "https://maven2.fabricmc.net"];
+async function l() {
+  return a(r, "/v2/versions/installer");
 }
 async function u() {
-  return a("https://maven.fabricmc.net/jdlist.txt").then((t) => t.split(`
+  return a(r, "/v2/versions/game");
+}
+async function m() {
+  return a(r, "/v2/versions/loader");
+}
+async function v() {
+  return a(r, "/v2/versions/yarn");
+}
+async function p(e, n) {
+  return a(r, `/v2/versions/loader/${e}/${n}/profile/json`);
+}
+async function y() {
+  return i(o, "/jdlist.txt").then((e) => e.split(`
 `));
 }
-async function v(t) {
-  return (await n(
-    `https://meta.fabricmc.net/v2/versions/yarn/${t}?limit=1`
+async function g(e) {
+  return (await a(
+    r,
+    `/v2/versions/yarn/${e}?limit=1`
   ))[0];
 }
-async function p() {
-  let t = await a("https://maven.fabricmc.net/net/fabricmc/fabric-api/fabric-api/maven-metadata.xml"), s = new DOMParser().parseFromString(t, "text/xml");
-  return Array.from(s.getElementsByTagName("version")).map((r) => r.childNodes[0].nodeValue);
+async function d() {
+  let e = await i(o, "/net/fabricmc/fabric-api/fabric-api/maven-metadata.xml"), s = new DOMParser().parseFromString(e, "text/xml");
+  return Array.from(s.getElementsByTagName("version")).map((f) => f.childNodes[0].nodeValue);
 }
-async function n(t) {
-  const e = await fetch(t);
-  if (e.ok)
-    return await e.json();
-  throw new Error(`Failed to fetch versions (Code: ${e.status})`);
+async function a(e, n) {
+  return await (await c(e, n)).json();
 }
-async function a(t) {
-  const e = await fetch(t);
-  if (e.ok)
-    return await e.text();
-  throw new Error(`Failed to fetch versions (Code: ${e.status})`);
+async function i(e, n) {
+  return await (await c(e, n)).text();
+}
+async function c(e, n) {
+  for (var s of e)
+    try {
+      const t = await fetch(s + n);
+      if (t.ok)
+        return t;
+      console.error(await t.text());
+    } catch (t) {
+      console.error(t);
+    }
+  throw new Error(`Failed to fetch: ${e[0] + n}`);
 }
 export {
-  m as a,
-  f as b,
-  o as c,
-  c as d,
-  v as e,
-  l as f,
-  u as g,
-  p as h
+  v as a,
+  m as b,
+  l as c,
+  u as d,
+  g as e,
+  p as f,
+  y as g,
+  d as h
 };
