b3ce30c7827e5be51ecbee7ca6f54fb013f4f780
modmuss50 <modmuss50@gmail.com>
2023-05-29 21:25:14 +0100

    More work on the CLI

--- cli/main.ts
+++ cli/main.ts
@@ -3,6 +3,9 @@ import * as generator from "../scripts/dist/fabric-template-generator.js";
 import { parse as parseXml } from "https://deno.land/x/xml@2.1.1/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 import * as path from "https://deno.land/std@0.177.1/path/mod.ts";
+import { colors } from "https://deno.land/x/cliffy@v0.25.7/ansi/mod.ts";
+
+const error = colors.bold.red;
 
 // Set the XML parser as we do not have DomParser here.
 generator.setXmlVersionParser((xml) => {
@@ -17,20 +20,22 @@ await new Command()
   })
   .option("-o, --output <outputDir>", "The output directory", {
     required: true,
+    default: "./",
   })
   .action(async ({ minecraftVersion, output }) => {
-    // Request write permission to the whole of the output directory.
-    const status = await Deno.permissions.request({
-      name: "write",
-      path: output,
-    });
-    if (status.state != "granted") {
-      return;
+    // If the dir is the current dir (default), check that it is empty before we go and create a mess.
+    if (output == "./") {
+      const isEmpty = await isDirEmpty(output);
+      if (!isEmpty) {
+        console.log(error("Current working directory is not empty"));
+        Deno.exit(2);
+      }
     }
+
+    await requestPermissions(output);
 
     console.log(`Generating template for Minecraft ${minecraftVersion}`);
 
-    // Actually genrate the template!
     await generator.generateTemplate({
       config: {
         modid: "example",
@@ -47,8 +52,46 @@ await new Command()
         },
       },
     });
+
+    console.log("Done!");
   })
   .parse();
+
+async function requestPermissions(outputDir: string) {
+  const permissions: Deno.PermissionDescriptor[] = [
+    {
+      name: "write",
+      path: outputDir,
+    },
+    {
+      name: "net",
+      host: "meta.fabricmc.net",
+    },
+    {
+      name: "net",
+      host: "maven.fabricmc.net",
+    },
+  ];
+
+  for (const permission of permissions) {
+    const status = await Deno.permissions.request(permission);
+
+    if (status.state != "granted") {
+      console.log("Permission not granted");
+      Deno.exit(1);
+    }
+  }
+}
+
+async function isDirEmpty(outputDir: string) {
+  const contents = Deno.readDir(outputDir);
+
+  for await (const _ of contents) {
+    return false;
+  }
+
+  return true;
+}
 
 async function writeFile(
   outputPath: string,
@@ -63,7 +106,7 @@ async function writeFile(
     mode: options?.unixPermissions,
   };
 
-  // TODO is there a cleaner way to do this?
+  // is there a cleaner way to do this?
   if (content instanceof String) {
     await Deno.writeTextFile(
       output,
