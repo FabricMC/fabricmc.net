10ce3355f5c3a9ea3b93b55ebbfee5d4ba7b9628
Tiphaine LAURENT <25824254+TiphaineLAURENT@users.noreply.github.com>
2023-06-22 22:35:04 +0200

    Remove unnecessary comparison

--- cli/commands/init.ts
+++ cli/commands/init.ts
@@ -26,7 +26,7 @@ export function initCommand() {
     .option("-y, --defaultOptions", "Generate a mod with default options")
     .arguments("[dir:file]")
     .action(async ({ defaultOptions }, dir: string | undefined) => {
-      await generate(defaultOptions == true, dir);
+      await generate(defaultOptions, dir);
     });
 }
 
