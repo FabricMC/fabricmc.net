fc3cb57abbe0bbc4d9c768042c67348e69197c55
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2023-09-30 10:00:41 -0400

    Fix fabric modid for certain older Minecraft versions. (#34)
    Co-authored-by: modmuss <modmuss50@gmail.com>

--- scripts/src/lib/template/modjson.ts
+++ scripts/src/lib/template/modjson.ts
@@ -5,6 +5,10 @@ import { getJavaVersion } from "./java"
 import { getMinorMinecraftVersion } from "./minecraft"
 import { decode64 } from './utils';
 
+function usesNewModid(fabricVersion: string) : boolean {
+  return Number(fabricVersion.split(".")[1]) >= 59;
+}
+
 export async function addModJson(writer: TemplateWriter, config: ComputedConfiguration) {
   var mixins = [
     ...await generateMixin(writer, config),
@@ -39,7 +43,7 @@ export async function addModJson(writer: TemplateWriter, config: ComputedConfigu
     }
   }
 
-  fabricModJson.depends[getMinorMinecraftVersion(config.minecraftVersion) >= 16 ? "fabric-api" : "fabric"] = "*"
+  fabricModJson.depends[usesNewModid(config.fabricVersion) ? "fabric-api" : "fabric"] = "*"
 
   if (config.kotlin) {
     fabricModJson.depends = {
