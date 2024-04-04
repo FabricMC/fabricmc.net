9065b7186e6919a566f3dd61772a3a8bd938c331
Calum H <hendersoncal117@gmail.com>
2024-04-04 16:26:49 +0000

    Develop page redesign.

--- scripts/src/lib/Documentation.svelte
+++ scripts/src/lib/Documentation.svelte
@@ -5,6 +5,14 @@
         getLoaderVersions,
     } from "./Api";
 
+		function handleSelectChange(event: any, project: any) {
+				const selectedVersion = event.target.value;
+
+				if (selectedVersion.includes("Select")) return;
+
+				window.open(`https://maven.fabricmc.net/docs/${project.prefix}${selectedVersion}/`, '_blank');
+		}
+
     function filterAndSortVersions(
         versions: string[],
         prefix: string,
@@ -31,7 +39,7 @@
 
         return [
             {
-                name: "Minecraft (yarn mappings)",
+                name: "Minecraft (Yarn)",
                 desc: "Javadoc documentation for Minecraft generated from the comments in the yarn mappings.",
                 prefix: "yarn-",
                 versions: filterAndSortVersions(
@@ -39,14 +47,14 @@
                     "yarn-",
                     yarnVersions.map((v) => v.version)
                 ),
-                selected: yarnVersions[0].version,
+                selected: "Select Version",
             },
             {
                 name: "Fabric API",
                 desc: "Javadoc documentation for Fabric API",
                 prefix: "fabric-api-",
                 versions: apiVersions,
-                selected: apiVersions[0],
+                selected: "Select Version",
             },
             {
                 name: "Fabric Loader",
@@ -57,7 +65,7 @@
                     "fabric-loader-",
                     loaderVersions.map((v) => v.version)
                 ),
-                selected: loaderVersions[0].version,
+                selected: "Select Version",
             },
             // Disabled for now as the mixin JD css seems broken
             // {
@@ -76,29 +84,15 @@
 {#await data}
     <p>Loading versions..</p>
 {:then data}
-    <p>Online Javadoc is available for each project and the game itself using the links below, but also through your IDE directly.</p>
     {#each data as project}
-        <!-- <h5>{project.name}</h5> -->
-        <!-- <p>{project.desc}</p> -->
-
-        <p>
-            {project.name}:
-            <select bind:value={project.selected} style="min-width: 200px">
-                {#each project.versions as version}
-                    <option value={version}>{version}</option>
-                {/each}
-            </select>
-
-            <a
-            class="jdbutton"
-            href={"https://maven.fabricmc.net/docs/" +
-                project.prefix +
-                project.selected +
-                "/"}
-            >
-                Go to JavaDoc
-            </a>
-        </p>
+        <div class="javadoc-selector">
+					<select value="Select {project.name} Version" on:change={(event) => handleSelectChange(event, project)}>
+						<option>Select {project.name} Version</option>
+							{#each project.versions as version}
+									<option value={version}>{version}</option>
+							{/each}
+					</select>
+				</div>
     {/each}
 {:catch error}
     <p style="color: red">Error: {error.message}</p>
