c5496659e5ea2ca86ac1f19d4c7a16a01a9127d7
modmuss50 <modmuss50@gmail.com>
2024-08-24 21:14:16 +0100

    1.12.2

--- scripts/src/lib/Api.ts
+++ scripts/src/lib/Api.ts
@@ -101,7 +101,7 @@ export function isApiVersionvalidForMcVersion(apiVersion: string, mcVersion: str
     if (mcVersion.startsWith("22w13oneblockatatime")) {
         branch = "22w13oneblockatatime"
     } else if (mcVersion.startsWith("24w")) {
-        branch = "1.21"
+        branch = "1.21.2"
     } else if (mcVersion.startsWith("23w")) {
         branch = "1.20.5"
     } else if (mcVersion.startsWith("22w")) {
@@ -128,6 +128,8 @@ export function isApiVersionvalidForMcVersion(apiVersion: string, mcVersion: str
         branch = "1.20.5"
     } else if (mcVersion.startsWith("1.20.6")) {
         branch = "1.20.6"
+    } else if (mcVersion.startsWith("1.21.2")) {
+        branch = "1.21.2"
     } else if (mcVersion.startsWith("1.21.1")) {
         branch = "1.21.1"
     } else if (mcVersion.startsWith("1.21")) {
