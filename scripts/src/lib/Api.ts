c4eed9093837a4e14510378042eb34828f417fa9
modmuss50 <modmuss50@gmail.com>
2025-01-13 08:54:53 +0000

    1.21.5

--- scripts/src/lib/Api.ts
+++ scripts/src/lib/Api.ts
@@ -100,6 +100,8 @@ export function isApiVersionvalidForMcVersion(apiVersion: string, mcVersion: str
     // Very dumb but idk of a better (easy) way.
     if (mcVersion.startsWith("22w13oneblockatatime")) {
         branch = "22w13oneblockatatime"
+    } else if (mcVersion.startsWith("25w")) {
+        branch = "1.21.5"
     } else if (mcVersion.startsWith("24w")) {
         branch = "1.21.4"
     } else if (mcVersion.startsWith("23w")) {
@@ -128,6 +130,8 @@ export function isApiVersionvalidForMcVersion(apiVersion: string, mcVersion: str
         branch = "1.20.5"
     } else if (mcVersion.startsWith("1.20.6")) {
         branch = "1.20.6"
+    } else if (mcVersion.startsWith("1.21.5")) {
+        branch = "1.21.5"
     } else if (mcVersion.startsWith("1.21.4")) {
         branch = "1.21.4"
     } else if (mcVersion.startsWith("1.21.3")) {
