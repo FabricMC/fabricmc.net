1b600430ac57b75ee73090c8b999dd124554beff
CelDaemon <celdaemon@voidgroup.net>
2026-02-28 00:00:05 +0100

    Set project name instead of artifact name

--- scripts/src/lib/template/gradlegroovy.ts
+++ scripts/src/lib/template/gradlegroovy.ts
@@ -2,10 +2,10 @@ import type { ComputedConfiguration, TemplateWriter } from './template';
 import { renderTemplate } from './eta';
 
 import buildGradleTemplate from './templates/gradle/groovy/build.gradle.eta?raw';
-import settingsGradle from './templates/gradle/groovy/settings.gradle?raw';
+import settingsGradleTemplate from './templates/gradle/groovy/settings.gradle.eta?raw';
 import { getJavaVersion } from './java';
 
 export async function addGroovyGradle(writer: TemplateWriter, config: ComputedConfiguration) {
 	await writer.write('build.gradle', renderTemplate(buildGradleTemplate, {...config, java: getJavaVersion(config.minecraftVersion)}));
-	await writer.write('settings.gradle', settingsGradle);
+	await writer.write('settings.gradle', renderTemplate(settingsGradleTemplate, config));
 }
