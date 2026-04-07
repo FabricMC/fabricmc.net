ee3ee4af4a7d1a1d84610ef8ffc239c71d3abde5
CelDaemon <celdaemon@voidgroup.net>
2026-04-07 11:52:16 +0200

    Remove extra dependencies

--- .github/scripts/template-versions.ts
+++ .github/scripts/template-versions.ts
@@ -1,12 +1,14 @@
+// @deno-types="../../scripts/dist/fabric-template-generator.d.ts"
 import { getGameVersions, getMajorMinecraftVersion, getMinorMinecraftVersion } from '../../scripts/dist/fabric-template-generator.js';
-import { setOutput } from 'npm:@actions/core';
 
 const stableVersions = (await getGameVersions()).filter(x => x.stable);
-const latestVersions = stableVersions.reduce((acc, x) => {
+
+const latestVersions = stableVersions.reduce((acc: Record<string, string>, x) => {
     const major = getMajorMinecraftVersion(x.version);
     const minor = getMinorMinecraftVersion(x.version);
     acc[`${major}.${minor}`] ??= x.version;
     return acc;
 }, {});
 
-setOutput('result', latestVersions);
+const result = Object.entries(latestVersions).map(([branch, version]) => ({ branch, version }));
+console.log(JSON.stringify(result));
