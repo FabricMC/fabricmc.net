16fba4db7803517137c8b6cac4a661a6062078a4
CelDaemon <celdaemon@voidgroup.net>
2026-04-08 14:13:26 +0200

    Use version for branch name directly

--- .github/scripts/template-versions.ts
+++ .github/scripts/template-versions.ts
@@ -1,7 +1,4 @@
 // @deno-types="../../scripts/dist/fabric-template-generator.d.ts"
 import { getTemplateGameVersions } from '../../scripts/dist/fabric-template-generator.js';
 
-const stableVersions = (await getTemplateGameVersions()).filter(x => x.stable);
-
-const result = stableVersions.map(({version}) => ({ branch: version, version }));
-console.log(JSON.stringify(result));
+console.log(JSON.stringify((await getTemplateGameVersions()).filter(x => x.stable).map(x => x.version)));
